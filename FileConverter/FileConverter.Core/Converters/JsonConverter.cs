47a64a7fa1a33c6a9fea093b68226f7eeb24c96b
ringj <ringj@DESKTOP-V882IGA>
2021-06-23 23:23:51 +0100

    added json converter test

---
+++ FileConverter/FileConverter.Core/Converters/JsonConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FileConverter.Core.Converters
+{
+    public class JsonConverter : IConverter
+    {
+        public bool Validate(string source) => 
+            source.Contains("{") && 
+            source.Contains("}") && 
+            source.Count(c => c == '{') == source.Count(c => c == '}');
+
+        public string ConvertFromIntermediateModel(Dictionary<string, object> source)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<string, object> ConvertToIntermediateModel(string source)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ FileConverter/FileConverter.UnitTests/Converters/JsonConverterTest.cs
@@ -0,0 +1,31 @@
+﻿using FileConverter.Core.Converters;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace FileConverter.UnitTests
+{
+    [TestClass]
+    public class JsonConverterTest
+    {
+        [TestMethod]
+        public void GIVEN_SourceWithoutChildrenProperties_WHEN_ConvertToJsonFormat_THEN_JsonFormatShouldBeCorrect()
+        {
+            //Arrange
+            var intermediateModel = new Dictionary<string, object> { 
+                { "p1", "v1" },
+                { "p2", "v2" },
+                { "p3", "v3" }
+            };
+            var converter = new JsonConverter();
+
+            //Act
+            var jsonFormat = converter.ConvertFromIntermediateModel(intermediateModel);
+
+
+            //Assert
+            jsonFormat.Should().Be("{\"p1\":\"v1\",\"p2\":\"v2\",\"p3\":\"v3\"}");
+        }
+    }
+}
