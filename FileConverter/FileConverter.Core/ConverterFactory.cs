ceab293b24d33600b0fe641cb9af91ef238c1080
ringj <ringj@DESKTOP-V882IGA>
2021-06-24 00:14:53 +0100

    added converter factory

---
+++ FileConverter/FileConverter.Core/ConverterFactory.cs
@@ -0,0 +1,18 @@
+﻿using FileConverter.Core.Converters;
+using System;
+
+
+namespace FileConverter.Core
+{
+    public class ConverterFactory
+    {
+        private readonly JsonConverter _jsonConverter;
+        private readonly CsvConverter _csvConverter;
+        private readonly XmlConverter _xmlConverter;
+        public IConverter GetConverter(Format format)
+        {
+            throw new NotImplementedException();
+
+        }
+    }
+}
---
+++ FileConverter/FileConverter.Core/Converters/XmlConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FileConverter.Core.Converters
+{
+    public class XmlConverter : IConverter
+    {
+        public string ConvertFromIntermediateModel(Dictionary<string, object> source)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<string, object> ConvertToIntermediateModel(string source)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Validate(string source)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
