faa600a051e0f8f1c3586e8dca31920fe15bd61a
ringj <ringj@DESKTOP-V882IGA>
2021-06-24 22:10:09 +0100

    fixed converter factory test

--- FileConverter/FileConverter.Core/ConverterFactory.cs
+++ FileConverter/FileConverter.Core/ConverterFactory.cs
@@ -1,6 +1,6 @@
 ﻿using FileConverter.Core.Converters;
 using System;
-
+using System.ComponentModel;
 
 namespace FileConverter.Core
 {
@@ -9,9 +9,27 @@ namespace FileConverter.Core
         private readonly JsonConverter _jsonConverter;
         private readonly CsvConverter _csvConverter;
         private readonly XmlConverter _xmlConverter;
+
+        public ConverterFactory(JsonConverter jsonConverter, CsvConverter csvConverter, XmlConverter xmlConverter)
+        {
+            _jsonConverter = jsonConverter ?? throw new ArgumentNullException(nameof(jsonConverter));
+            _csvConverter = csvConverter ?? throw new ArgumentNullException(nameof(csvConverter));
+            _xmlConverter = xmlConverter ?? throw new ArgumentNullException(nameof(xmlConverter));
+        }
+
         public IConverter GetConverter(Format format)
         {
-            throw new NotImplementedException();
+            switch (format)
+            {
+                case Format.Csv:
+                    return _csvConverter;
+                case Format.Json:
+                    return _jsonConverter;
+                case Format.Xml:
+                    return _xmlConverter;
+                default:
+                    throw new InvalidEnumArgumentException();
+            }
 
         }
     }
