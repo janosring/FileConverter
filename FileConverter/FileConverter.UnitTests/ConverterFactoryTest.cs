b5fa94a2722dd76ffdda03bf4f58b15ca3b82361
ringj <ringj@DESKTOP-V882IGA>
2021-06-24 22:09:25 +0100

    added ConverterFactory test

---
+++ FileConverter/FileConverter.UnitTests/ConverterFactoryTest.cs
@@ -0,0 +1,26 @@
+﻿using FileConverter.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Autofac.Extras.Moq;
+using FluentAssertions;
+using FileConverter.Core.Converters;
+
+namespace FileConverter
+{
+    [TestClass]
+    public class ConverterFactoryTest
+    {
+        [TestMethod]
+        public void GIVEN_CreateConverter_WHEN_TypeIsCsv_THEN_ConverterTypeShouldBeCsvConverter()
+        {
+            //Arrange
+            using var mock = AutoMock.GetLoose();
+            var factory = mock.Create<ConverterFactory>();
+
+            //Act
+            var converter = factory.GetConverter(Format.Csv);
+
+            //Assert
+            converter.Should().BeOfType<CsvConverter>();
+        }
+    }
+}
