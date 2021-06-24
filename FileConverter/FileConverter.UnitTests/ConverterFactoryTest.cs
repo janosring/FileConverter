213896804e8ca729021d39753b2ea0eaa1f43b23
ringj <ringj@DESKTOP-V882IGA>
2021-06-24 22:19:38 +0100

    added converter factory tests

--- FileConverter/FileConverter.UnitTests/ConverterFactoryTest.cs
+++ FileConverter/FileConverter.UnitTests/ConverterFactoryTest.cs
@@ -14,13 +14,57 @@ namespace FileConverter
         {
             //Arrange
             using var mock = AutoMock.GetLoose();
-            var factory = mock.Create<ConverterFactory>();
+
+            var csvConverter = mock.Create<CsvConverter>();
+            var jsonConverter = mock.Create<JsonConverter>();
+            var xmlCoverter = mock.Create<XmlConverter>();
+
+            var factory = new ConverterFactory(jsonConverter, csvConverter, xmlCoverter);
 
             //Act
             var converter = factory.GetConverter(Format.Csv);
 
             //Assert
             converter.Should().BeOfType<CsvConverter>();
+        }
+
+        [TestMethod]
+        public void GIVEN_CreateConverter_WHEN_TypeIsJson_THEN_ConverterTypeShouldBeJsonConverter()
+        {
+            //Arrange
+            using var mock = AutoMock.GetLoose();
+
+            var csvConverter = mock.Create<CsvConverter>();
+            var jsonConverter = mock.Create<JsonConverter>();
+            var xmlCoverter = mock.Create<XmlConverter>();
+
+            var factory = new ConverterFactory(jsonConverter, csvConverter, xmlCoverter);
+
+            //Act
+            var converter = factory.GetConverter(Format.Json);
+
+            //Assert
+            converter.Should().BeOfType<JsonConverter>();
+        }
+
+
+        [TestMethod]
+        public void GIVEN_CreateConverter_WHEN_TypeIsXml_THEN_ConverterTypeShouldBeXmlConverter()
+        {
+            //Arrange
+            using var mock = AutoMock.GetLoose();
+
+            var csvConverter = mock.Create<CsvConverter>();
+            var jsonConverter = mock.Create<JsonConverter>();
+            var xmlCoverter = mock.Create<XmlConverter>();
+
+            var factory = new ConverterFactory(jsonConverter, csvConverter, xmlCoverter);
+
+            //Act
+            var converter = factory.GetConverter(Format.Xml);
+
+            //Assert
+            converter.Should().BeOfType<XmlConverter>();
         }
     }
 }
