25375ac383cf724f3e22c93197b9eba294d3f216
ringj <ringj@DESKTOP-V882IGA>
2021-06-29 23:35:32 +0100

    fixed test

--- FileConverter/FileConverter.Core/Converters/JsonConverter.cs
+++ FileConverter/FileConverter.Core/Converters/JsonConverter.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace FileConverter.Core.Converters
 {
@@ -45,7 +45,77 @@ namespace FileConverter.Core.Converters
 
         public Dictionary<string, object> ConvertToIntermediateModel(string source)
         {
-            throw new NotImplementedException();
+            var intermediateModel = new Dictionary<string, object>();
+            source = RemoveFirstCharacter(source, "{");
+            source = RemoveLastCharacter(source, "}");
+
+            ConvertNode(source);
+            return intermediateModel;
+
+            void ConvertNode(string node) 
+            {                                
+                var nodeNamePattern = "^\"[\\w|\\n]*\"";
+
+                var regex = new Regex(nodeNamePattern);
+
+                var match = regex.Match(node);
+                var propertyName = match.Value;
+                var restOfTheNode = node.Replace(propertyName, string.Empty);
+
+                var properyValues = FindProperyValue(restOfTheNode);
+                var properyValue = properyValues.Split(',').FirstOrDefault();
+
+
+                intermediateModel.Add(propertyName.Replace("\"", string.Empty), properyValue.Replace("\"", string.Empty));
+                restOfTheNode = node.Replace($"{propertyName}:{properyValue},", string.Empty);
+                restOfTheNode = restOfTheNode.Replace($"{propertyName}:{properyValue}", string.Empty);
+
+                if (!string.IsNullOrWhiteSpace(restOfTheNode)) ConvertNode(restOfTheNode);
+            }           
+
+            string FindProperyValue(string propery) 
+            {
+                propery = RemoveFirstCharacter(propery, ":");
+                if (!propery.Contains("{")) 
+                    return propery;
+
+                var numberOfOpenBrackets = 0;
+                var numberOfCloseBrackets = 0;
+                var propertyValue = new StringBuilder();
+
+                foreach (var item in propery)
+                {
+                    propertyValue.Append(item);
+
+                    if (item == '{')
+                        numberOfOpenBrackets++;
+
+                    else if (item == '}')
+                        numberOfCloseBrackets++;
+
+                    if(numberOfCloseBrackets != 0 && numberOfOpenBrackets == numberOfCloseBrackets)
+                    {
+                        break;
+                    }
+                }
+
+                return propertyValue.ToString();
+            }
+
+            string RemoveFirstCharacter(string src, string characterToRemove)
+            {
+                var index = src.IndexOf(characterToRemove);
+                return (index < 0)
+                    ? src
+                    : src.Remove(index, 1);
+            }
+            
+            string RemoveLastCharacter(string src, string characterToRemove)
+            {
+                var index = src.LastIndexOf(characterToRemove);
+                return
+                    src.Substring(0, index > -1 ? index : src.Count());
+            }
         }
 
     }
