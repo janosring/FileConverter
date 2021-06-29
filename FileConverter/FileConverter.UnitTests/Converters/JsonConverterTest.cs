5c2ea51812959478637b00ddf6394be40b7a4f7e
ringj <ringj@DESKTOP-V882IGA>
2021-06-29 23:34:58 +0100

    added json to intermediate converter test

--- FileConverter/FileConverter.UnitTests/Converters/JsonConverterTest.cs
+++ FileConverter/FileConverter.UnitTests/Converters/JsonConverterTest.cs
@@ -46,5 +46,35 @@ namespace FileConverter.UnitTests
             //Assert
             jsonFormat.Should().Be("{\"p1\":{\"p1.1\":\"v1.1\",\"p1.2\":\"v1.2\"},\"p2\":\"v2\",\"p3\":\"v3\"}");
         }
+
+        [TestMethod]
+        public void GIVEN_SourceWithoutChildrenProperties_WHEN_ConvertToIntermediateModel_THEN_ModelShouldBeCorrect()
+        {
+            //Arrange
+            var source = "{\"p1\":\"v1\",\"p2\":\"v2\",\"p3\":\"v3\"}";
+            var converter = new JsonConverter();
+
+            //Act
+            var intermediateModel = converter.ConvertToIntermediateModel(source);
+
+
+            //Assert
+            intermediateModel.Should().NotBeNull();
+
+            intermediateModel
+                .Should().ContainKey("p1")
+                .WhichValue.Should().BeOfType<string>()
+                .Which.Should().BeEquivalentTo("v1");
+
+            intermediateModel
+                .Should().ContainKey("p2")
+                .WhichValue.Should().BeOfType<string>()
+                .Which.Should().BeEquivalentTo("v2");
+
+            intermediateModel
+                .Should().ContainKey("p3")
+                .WhichValue.Should().BeOfType<string>()
+                .Which.Should().BeEquivalentTo("v3");
+        }
     }
 }
