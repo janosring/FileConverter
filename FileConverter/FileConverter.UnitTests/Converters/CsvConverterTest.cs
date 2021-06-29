82afc411272c8b3b0092be4e34e0090e4273ae8b
ringj <ringj@DESKTOP-V882IGA>
2021-06-29 22:11:25 +0100

    added new intermediate -> csv converter test

--- FileConverter/FileConverter.UnitTests/Converters/CsvConverterTest.cs
+++ FileConverter/FileConverter.UnitTests/Converters/CsvConverterTest.cs
@@ -43,7 +43,7 @@ namespace FileConverter.UnitTests
         {
             //Arrange
             var converter = new CsvConverter();
-            var source = $"p1,p2_p2p1,p2_p2p2,p3,p3_p3p1,p4{Environment.NewLine}v1,v2.1,v2.2,v3,v3.1,v4";
+            var source = $"p1,p2_p2p1,p2_p2p2,p3_p3p1,p4{Environment.NewLine}v1,v2.1,v2.2,v3.1,v4";
 
             //Act
             var intermediateModel = converter.ConvertToIntermediateModel(source);
@@ -67,12 +67,6 @@ namespace FileConverter.UnitTests
                 .WhichValue.Should().BeOfType<Dictionary<string, object>>()
                 .Which.Should().ContainKey("p2p2")
                 .WhichValue.Should().BeEquivalentTo("v2.2");
-
-            intermediateModel
-               .Should().ContainKey("p3")
-                .WhichValue.Should().BeOfType<Dictionary<string, object>>()
-                .Which.Should().ContainKey("p3")
-                .WhichValue.Should().BeEquivalentTo("v3");
 
             intermediateModel
                .Should().ContainKey("p3")
@@ -102,6 +96,26 @@ namespace FileConverter.UnitTests
 
             //Assert
             intermediateModel.Should().Be($"p1,p2,p3{Environment.NewLine}v1,v2,v3");
+        }
+
+        [TestMethod]
+        public void GIVEN_SourceWithChildrenProperties_WHEN_ConvertToCsvFormat_THEN_CsvShouldBeCorrect()
+        {
+            //Arrange
+            var converter = new CsvConverter();
+            var source = new Dictionary<string, object>()
+            {
+                { "p1", "v1"},
+                {"p2", new Dictionary<string,object>(){ {"p2p1", "v2.1" }, {"p2p2", "v2.2" } } },
+                {"p3", new Dictionary<string,object>(){ {"p3p1", "v3.1" }} },
+                {"p4", "v4" }
+            };
+
+            //Act
+            var intermediateModel = converter.ConvertFromIntermediateModel(source);
+
+            //Assert
+            intermediateModel.Should().Be($"p1,p2_p2p1,p2_p2p2,p3_p3p1,p4{Environment.NewLine}v1,v2.1,v2.2,v3.1,v4");
         }
     }
 }
