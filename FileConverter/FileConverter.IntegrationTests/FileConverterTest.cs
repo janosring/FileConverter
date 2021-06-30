66268912077e70904759c003f6ce3936b00e3469
ringj <ringj@DESKTOP-V882IGA>
2021-06-30 22:47:29 +0100

    Added csv -> xml converter integration test

--- FileConverter/FileConverter.IntegrationTests/FileConverterTest.cs
+++ FileConverter/FileConverter.IntegrationTests/FileConverterTest.cs
@@ -26,7 +26,25 @@ namespace FileConverter.IntegrationTests
 
             //Assert
             target.Should().Be("{\"name\":\"Dave\",\"address\":{\"line1\":\"Street\",\"line2\":\"Town\"}}");
+        }
 
+        [TestMethod]
+        public void GIVEN_SourceIsInCSVFormat_WHEN_ConvertItToXml_THEN_TargetShouldBeTheCorrect()
+        {
+            //Arrange
+            var source = $"name,address_line1,address_line2{Environment.NewLine}Dave,Street,Town";
+            var targetFormat = Format.Xml;
+
+            var services = Setup.ConfigureServices();
+            var serviceProvider = services.BuildServiceProvider();
+
+            var converter = serviceProvider.GetService<IFileConverter>();
+
+            //Act
+            var target = converter.Convert(source, targetFormat);
+
+            //Assert
+            target.Should().Be($"<?xml version=\"1.0\" encoding=\"UTF-8\" ?>{Environment.NewLine}<root>{Environment.NewLine}<name>Dave</name>{Environment.NewLine}<address>{Environment.NewLine}<line1>Street</line1>{Environment.NewLine}<line2>Town</line2>{Environment.NewLine}</address>{Environment.NewLine}</root>");
         }
 
         [TestMethod]
